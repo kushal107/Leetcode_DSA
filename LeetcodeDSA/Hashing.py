746ac3a027ae07a55ee8393e766b96b12d3ae5c9
kushal107 <kushal107@users.noreply.github.com>
2026-06-16 18:10:43 +0530

    june 16

---
+++ LeetcodeDSA/Hashing.py
@@ -0,0 +1,11 @@
+hash_map = {11:'a', 22:'b'}
+hash_map[33] = 'c'
+print(hash_map)
+
+for k in hash_map:
+    print(k)
+for v in hash_map.values():
+    print(v)
+
+for k,v in hash_map.items():
+    print(k,v)
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
+
 # stripe_interview_practice
-=======
-Leetcode learning
->>>>>>> 0b0b7286b069ed40ced674726a1d047e651dd5bc
+
